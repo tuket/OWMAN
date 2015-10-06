a87205333128ff62b8509fee3fc989a90d4bd2ef
tuket <tuketelamodelmon@gmail.com>
2015-10-06 23:08:05 +0200

    added sprite_status

---
+++ src/renderer/sprite_status.cpp
@@ -0,0 +1,1 @@
+#include "sprite_status.hpp"
---
+++ src/renderer/sprite_status.hpp
@@ -0,0 +1,15 @@
+#ifndef SPRITE_STATUS
+#define SPRITE_STATUS
+
+class SpriteStatus
+{
+
+    friend class SpriteManager;
+
+    int currentAnimation;
+    int currentFrame;
+    float currentFrameTime;
+
+};
+
+#endif // SPRITE_STATUS
