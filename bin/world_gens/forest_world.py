3b5546b05788cbe77fb20a7606b72cda20c728e7
tuket <tuketelamodelmon@gmail.com>
2016-03-04 15:53:54 +0100

    forest world generator

---
+++ bin/world_gens/forest_world.py
@@ -0,0 +1,38 @@
+dir_temp = "../cell_templates/"
+dir_world = "../world_folder/"
+world_file_name = dir_world + "world_file.txt"
+num_temps = 20
+
+from random import randint
+
+temps = []
+for i in xrange(num_temps):
+	t = dir_temp + str(i)
+	temps.append(t)
+	
+w = 200
+h = 200
+
+wfc = ""
+world_file = open(world_file_name, "w")
+
+for y in xrange(w):
+	for x in xrange(h):
+		rand_cell = randint(0, num_temps-1)
+		
+		name_source = dir_temp + "cell_" + str(rand_cell) + ".xml"
+		name_dest = dir_world + "cell_" + str(x) + "_" + str(y) + ".xml"
+		
+		source_file = open(name_source, "r")
+		dest_file = open(name_dest, "w")
+		
+		content = source_file.read()
+		dest_file.write(content)
+		
+		source_file.close()
+		dest_file.close()
+		
+		wfc += str(x) + " " + str(y) + "\n"
+		
+world_file.write(wfc)
+world_file.close()
